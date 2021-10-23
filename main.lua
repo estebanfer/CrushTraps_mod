e55b3ab1a1addfd831782a5fb1e5b4ffc1f2d75f
estebanfer <estebanfer2004@gmail.com>
2021-10-23 05:18:54 -0300

    Now all entrance doors have a safe zone Changed spawn safe zone to a safe zone in the array, not one apart

--- main.lua
+++ main.lua
@@ -6,12 +6,11 @@ local floor_types = {ENT_TYPE.FLOOR_GENERIC, ENT_TYPE.FLOOR_JUNGLE, ENT_TYPE.FLO
 local zones = {}
 local tofix = {}
 local used = {}
-local entrance_door
 
 local function is_not_on_safe_zone(x, y)
     for i,zone in ipairs(zones) do
         local xdiff, ydiff = x-zone.x, y-zone.y
-        if math.sqrt(xdiff*xdiff+ydiff*ydiff) < options.f_safe_zone_radius then
+        if math.sqrt(xdiff*xdiff+ydiff*ydiff) < options.f_safe_zone_radius+0.5 then
             return false
         end
     end
@@ -26,16 +25,16 @@ local function insert_to_used(x, y)
     end
 end
 
-local function valid_crushblock_spawn(ent, uid, x, y, l, radius)
+local function valid_crushblock_spawn(ent, uid, x, y, l)
     local top_e_type = get_grid_entity_at(x, y+1, l)
     top_e_type = top_e_type == -1 or get_entity(top_e_type).type.id
-    return not test_flag(ent.flags, ENT_FLAG.SHOP_FLOOR) and distance(entrance_door, uid) > radius and is_not_on_safe_zone(x, y) and top_e_type ~= ENT_TYPE.FLOOR_ALTAR and top_e_type ~= ENT_TYPE.FLOOR_EGGPLANT_ALTAR
+    return not test_flag(ent.flags, ENT_FLAG.SHOP_FLOOR) and is_not_on_safe_zone(x, y) and top_e_type ~= ENT_TYPE.FLOOR_ALTAR and top_e_type ~= ENT_TYPE.FLOOR_EGGPLANT_ALTAR
 end
 
-local function valid_crushblock_l_spawn(ent, uid, x, y, l, radius) --made for large crushblock
+local function valid_crushblock_l_spawn(ent, uid, x, y, l) --made for large crushblock
     local top_e_type = get_grid_entity_at(x, y+1, l)
     top_e_type = top_e_type == -1 or get_entity(top_e_type).type.id
-    return not test_flag(ent.flags, ENT_FLAG.SHOP_FLOOR) and distance(entrance_door, uid) > radius and is_not_on_safe_zone(x, y) and not (used[x] and used[x][y]) and ent.type.id > ENT_TYPE.FLOOR_BORDERTILE_OCTOPUS and ent.type.id ~= ENT_TYPE.FLOOR_PIPE and top_e_type ~= ENT_TYPE.FLOOR_ALTAR and top_e_type ~= ENT_TYPE.FLOOR_EGGPLANT_ALTAR
+    return not test_flag(ent.flags, ENT_FLAG.SHOP_FLOOR) and is_not_on_safe_zone(x, y) and not (used[x] and used[x][y]) and ent.type.id > ENT_TYPE.FLOOR_BORDERTILE_OCTOPUS and ent.type.id ~= ENT_TYPE.FLOOR_PIPE and top_e_type ~= ENT_TYPE.FLOOR_ALTAR and top_e_type ~= ENT_TYPE.FLOOR_EGGPLANT_ALTAR
 end
 
 local function destroy_floor(ent)
@@ -52,19 +51,19 @@ end
 
 set_callback(function()
     if state.theme == THEME.BASE_CAMP then return end
-    local radius
-    if state.theme == THEME.OLMEC then radius = options.c_spawn_zone_safe_radius_olmec
-    else radius = options.b_spawn_zone_safe_radius end
     zones = {}; used = {}; tofix = {}
-    if options.d_spawn_zone_safe_radius then
+    if options.d_spawn_safe_zones then
         local max_x = state.width*10+2
         local max_y = state.height*8+90
         for i=1, state.width*state.height/options.e_safe_zone_divisor do
             zones[i] = {['x'] = prng:random_int(2, max_x, PRNG_CLASS.PROCEDURAL_SPAWNS), ['y'] = prng:random_int(90, max_y, PRNG_CLASS.PROCEDURAL_SPAWNS)}--math.random(2, state.width*10+2), ['y'] = math.random(90, state.height*8+90)}
         end
     end
-    entrance_door = get_entities_by(ENT_TYPE.FLOOR_DOOR_ENTRANCE, MASK.ANY, LAYER.FRONT)[1]
-    local px, py, pl = get_position(entrance_door)
+    entrance_doors = get_entities_by_type(ENT_TYPE.FLOOR_DOOR_ENTRANCE)
+    for _,uid in ipairs(entrance_doors) do
+        local dx, dy, _ = get_position(uid)
+        zones[#zones+1] = {['x'] = dx, ['y'] = dy}
+    end
     local floors = get_entities_by(floor_types, 0, LAYER.FRONT)
     local spawn_chance = options.a1_spawn_chance / 20
     local large_spawn_chance = options.a2_large_spawn_chance / 20
@@ -72,7 +71,7 @@ set_callback(function()
     for _,uid in ipairs(floors) do
         local ent = get_entity(uid)
         local x, y, l = get_position(uid)
-        if prng:random_float(PRNG_CLASS.PROCEDURAL_SPAWNS) < spawn_chance and valid_crushblock_spawn(ent, uid, x, y, l, radius) then
+        if prng:random_float(PRNG_CLASS.PROCEDURAL_SPAWNS) < spawn_chance and valid_crushblock_spawn(ent, uid, x, y, l) then
             if not (used[x] and used[x][y]) then
                 if prng:random_float(PRNG_CLASS.PROCEDURAL_SPAWNS) < large_spawn_chance then
                     local right_uid = get_grid_entity_at(x+1, y, l)
@@ -80,9 +79,9 @@ set_callback(function()
                     local down_right_uid = get_grid_entity_at(x+1, y-1, l)
                     if right_uid ~= -1 and down_uid ~= -1 and down_right_uid ~= -1 then
                         local right, down, down_right = get_entity(right_uid), get_entity(down_uid), get_entity(down_right_uid)
-                        if test_flag(right.flags, ENT_FLAG.SOLID) and valid_crushblock_l_spawn(right, right_uid, x+1, y, l, radius) and
-                        test_flag(down.flags, ENT_FLAG.SOLID) and valid_crushblock_l_spawn(down, down_uid, x, y-1, l, radius) and
-                        test_flag(down_right.flags, ENT_FLAG.SOLID) and valid_crushblock_l_spawn(down_right, down_right_uid, x+1, y-1, l, radius) then
+                        if test_flag(right.flags, ENT_FLAG.SOLID) and valid_crushblock_l_spawn(right, right_uid, x+1, y, l) and
+                        test_flag(down.flags, ENT_FLAG.SOLID) and valid_crushblock_l_spawn(down, down_uid, x, y-1, l) and
+                        test_flag(down_right.flags, ENT_FLAG.SOLID) and valid_crushblock_l_spawn(down_right, down_right_uid, x+1, y-1, l) then
                             destroy_floor(ent); destroy_floor(right); destroy_floor(down); destroy_floor(down_right)
                             spawn(ENT_TYPE.ACTIVEFLOOR_CRUSH_TRAP_LARGE, x+0.5, y-0.5, l, 0, 0)
                             insert_to_used(x, y); insert_to_used(x+1, y); insert_to_used(x, y-1) insert_to_used(x+1, y-1)
@@ -102,7 +101,7 @@ set_callback(function()
     end
     if options.g_fix_floors then
         set_timeout(function()
-            for i,uid in ipairs(tofix) do
+            for _,uid in ipairs(tofix) do
                 local ent = get_entity(uid)
                 if ent then
                     if ent.type.id < ENT_TYPE.FLOORSTYLED_MINEWOOD then
@@ -118,9 +117,7 @@ end, ON.POST_LEVEL_GENERATION)
 
 register_option_int('a1_spawn_chance', 'Crush trap spawn chance', '0 is 0%, 20 is 100%', 15, 0, 20)
 register_option_int('a2_large_spawn_chance', 'Large crush trap chance', '0 is 0%, 20 is 100%', 5, 0, 20)
-register_option_int('b_spawn_zone_safe_radius', 'spawn zone safety radius', '', 6, 0, 12)
-register_option_int('c_spawn_zone_safe_radius_olmec', 'spawn zone safety radius (olmec level)', '', 3, 0, 12)
-register_option_bool('d_spawn_zone_safe_radius', 'enable safe zones', '', true)
+register_option_bool('d_spawn_safe_zones', 'enable safe zones', '(won\'t disable spawn safe zone)', true)
 register_option_int('e_safe_zone_divisor', 'Safe zones divisor', 'the number of safe zones is the amount of rooms, divided by this number', 6, 1, 10)
 register_option_int('f_safe_zone_radius', 'safe zones radius', '', 5, 0, 12)
 register_option_bool('g_fix_floors', 'Try to fix floors deco', '', true)
